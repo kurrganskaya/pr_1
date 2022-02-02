cebd6fc8e01af6150976c78ddc527cce848af49b
kurrganskaya <66162354+kurrganskaya@users.noreply.github.com>
2022-02-02 11:45:43 +0300

    Add files via upload

---
+++ task 2.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Даны два числа. Показать большее и меньшее число
+
+int number1 = 32 ; 
+int number2 = 53 ;
+
+if (number1 > number2)
+{
+    Console.WriteLine("max number1: " + number1);
+    Console.WriteLine("min number2: " + number2);
+}
+else
+{
+     Console.WriteLine("max number2: " + number2);
+     Console.WriteLine("min number1: " + number1);
+}
